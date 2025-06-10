89e09cf7a5b3857d6a4d8a21f548539c1e2c319f
Alexey Kiselev <alexey4050@mail.ru>
2025-06-10 19:32:16 +0300

    Fix sonar bags

--- app/src/main/java/hexlet/code/controller/RootController.java
+++ app/src/main/java/hexlet/code/controller/RootController.java
@@ -6,7 +6,12 @@ import io.javalin.http.Context;
 import static io.javalin.rendering.template.TemplateUtil.model;
 
 public class RootController {
+    private RootController() {
+        throw new UnsupportedOperationException("This is a utility class and cannot be instantiated");
+    }
+
     public static void index(Context ctx) {
+
         var page = new BasePage();
         String flashType = ctx.attribute("flashType");
         String flashMessage = ctx.attribute("flashMessage");
